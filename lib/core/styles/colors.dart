d6d12661af826da00ba5bbe8516445378f3568a8
farah <farah@farahs-MacBook-Pro.local>
2025-05-24 21:03:04 +0300

    setup app colors&assets

---
+++ lib/core/styles/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const Color primary =Color(0xff8000FF);
+  static const Color primaryDark =Color(0xff540BA1);
+  static const Color pink8A =Color(0xffFF1F8A);
+  static const Color greyCC =Color(0xffCCCCCC);
+  static const Color grey43 =Color(0xff303243);
+  static const Color black2E =Color(0xff21222E);
+
+}
